8cd3415d5cce4377314c746578d1a5155a56407e
Mika Koivisto <mika.koivisto@student.careeria.fi>
2022-06-10 13:22:55 +0300

    Product and supplier listing + adding works

--- app/urls.py
+++ app/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import landingview, productlistview, supplierlistview
+from .views import landingview, productlistview, supplierlistview, addsupplier, addproduct
 
 urlpatterns = [
     path('', landingview),
 
     # Products url's
     path('products/', productlistview),
+    path('add-product/', addproduct),
 
     # Suppliers url's
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
 ]
